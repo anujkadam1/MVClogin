10302e92738533d16869d2805132bf4bb1b7b61a
anuj kadam <anujkadam15@gmail.com>
2023-05-30 17:56:44 +0530

    firstCommit

---
+++ MvcLogin/Repositories/Abstract/IUserAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using Dotnet6MvcLogin.Models.DTO;
+
+namespace Dotnet6MvcLogin.Repositories.Abstract
+{
+    public interface IUserAuthenticationService
+    {
+   
+        Task<Status> LoginAsync(LoginModel model);
+        Task LogoutAsync();
+        Task<Status> RegisterAsync(RegistrationModel model);
+    }
+}
